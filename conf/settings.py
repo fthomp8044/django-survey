aca1e8237d8e2362d9794d8130b31e1ee2ec9d34
fthomp8044 <fthomp2430@gmail.com>
2019-10-14 12:17:54 -0400

    changes for deployment

--- conf/settings.py
+++ conf/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+
 
 
     #local party
